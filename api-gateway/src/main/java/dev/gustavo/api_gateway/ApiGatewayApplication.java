1ebc201eb318e4f192c059a2ecbcab2d8e74a976
gustavogeraldelli <geraldellig@gmail.com>
2025-08-31 15:33:18 -0300

    feat(gateway): configs and route

--- api-gateway/src/main/java/dev/gustavo/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/dev/gustavo/api_gateway/ApiGatewayApplication.java
@@ -2,12 +2,25 @@ package dev.gustavo.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator routes(RouteLocatorBuilder builder) {
+		return builder
+				.routes()
+					.route(r -> r.path("/clients/**").uri("lb://client-ms"))
+				.build();
 	}
 
 }
