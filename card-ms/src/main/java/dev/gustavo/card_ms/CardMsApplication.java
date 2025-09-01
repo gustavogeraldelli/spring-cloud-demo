66baaca83f11f3e1041bb8819f7ab19f0290fc30
gustavo <geraldellig@gmail.com>
2025-08-31 21:48:25 -0300

    init(card): initializr

---
+++ card-ms/src/main/java/dev/gustavo/card_ms/CardMsApplication.java
@@ -0,0 +1,13 @@
+package dev.gustavo.card_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardMsApplication.class, args);
+	}
+
+}
